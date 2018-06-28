af13a673c69303b2fe2497c8057a2434c4fd1c42
David Balzan <david.balzan@gmail.com>
2018-06-28 17:28:38 +0200

    initialize spring boot app via start.spring.io
    Initially will include web and websocket dependencies

---
+++ src/main/java/david/balzan/chatapp/ChatAppApplication.java
@@ -0,0 +1,12 @@
+package david.balzan.chatapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatAppApplication.class, args);
+	}
+}
---
+++ src/test/java/david/balzan/chatapp/ChatAppApplicationTests.java
@@ -0,0 +1,16 @@
+package david.balzan.chatapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ChatAppApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
