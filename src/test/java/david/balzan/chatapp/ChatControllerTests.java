4ec4fd48a9eadac2ab8e0a47a185f849aad43f6d
David Balzan <david.balzan@gmail.com>
2018-06-29 16:18:51 +0200

    Add simple tests to make sure that the controller is being initialized

---
+++ src/test/java/david/balzan/chatapp/ChatControllerTests.java
@@ -0,0 +1,36 @@
+package david.balzan.chatapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class ChatControllerTests {
+    @Autowired
+    private ChatController controller;
+
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void greetingShouldReturnDefaultMessage() throws Exception {
+        assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/ws",
+                String.class)).contains("Welcome to SockJS!");
+    }
+
+    @Test
+    public void contexLoads() throws Exception {
+        assertThat(controller).isNotNull();
+    }
+}
